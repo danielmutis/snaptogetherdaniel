855481bd4c79c6b421eb674a0bd5f6af4ca8c287
Canonical <canonical@Daniels-MacBook-Pro.local>
2020-02-06 14:28:13 +0000

    changes

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="snaptogetherdaniel",
+    name="togetherdaniel",
     version="0.0.1",
     author="Daniel Mutis",
     author_email="daniel.mutis@canonical.com",
