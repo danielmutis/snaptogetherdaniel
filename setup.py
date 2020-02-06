32e32de8c5c30eae35da75ef5ecf5225399b6401
Canonical <canonical@Daniels-MacBook-Pro.local>
2020-02-06 13:42:51 +0000

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env/python3
+
+from setuptools import setup, find_packages
+
+setup(
+    name="snaptogetherdaniel",
+    version="0.0.1",
+    author="Daniel Mutis",
+    author_email="daniel.mutis@canonical.com",
+    py_modules=["test_snap"],
+)
